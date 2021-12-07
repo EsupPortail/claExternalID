52638b5cf8a690c02cead9889fcc3f60bccd81fa
Adel Terki <Mohamed-Adel.Terki@univ-paris1.fr>
2021-12-07 11:37:32 +0100

    FranceConnect : identity reconciliation SI  pseudo ID pivot (Nom de naissance, prénom, sexe, date de naissance) & mail les mêmes (équivalents aux données présentes dans LDAP) alors : 	 reconciliation automatique (ne pas passer par la mire CAS de reconciliation, mettre le sub dans le profil de l'utilisateur, rediriger vers le service demandé) SINON : Afficher la mire de connexion Paris 1 (fonctionnement actuel)

--- src/main/java/org/esupportail/claExternalID/Ldap.java
+++ src/main/java/org/esupportail/claExternalID/Ldap.java
@@ -13,6 +13,10 @@ import javax.naming.directory.BasicAttributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.InvalidAttributeIdentifierException;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,13 +27,13 @@ import java.util.Collections;
 import javax.naming.directory.SearchResult;
 import java.util.ArrayList;
 import java.util.Enumeration;
-    
+
 class Ldap {
     @SuppressWarnings("serial")
     static class Attrs extends HashMap<String, List<String>> {}
 
     static class LdapConf {
-        String url, bindDN, bindPasswd, peopleDN, principalId;
+        String url, bindDN, bindPasswd, peopleDN, principalId, birthdate, mailPerso, civility, family_name, given_name;
     }
     LdapConf ldapConf;
     DirContext dirContext;
@@ -91,6 +95,53 @@ class Ldap {
             log.error("The user '" + result.getAttributes().get(ldapConf.principalId).get() + "' already has an '" + attr + "' with value '" + value + "'");
         }
         return valid;
+    }
+
+    NamingEnumeration<? extends SearchResult> getEntryForReconciliation(String filter) throws NamingException {
+      //(&(up1BirthDay=19500101000000Z)(supannMailPerso=johnS@yahoo.fr)(supannCivilite=M.)(up1BirthName=Smith)(givenName=John))
+        log.debug(filter);
+        SearchControls controls = new SearchControls();
+        controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        controls.setReturningAttributes(new String [] {
+                  ldapConf.principalId,
+                  ldapConf.mailPerso,
+                  ldapConf.birthdate,
+                  ldapConf.civility,
+                  ldapConf.family_name,
+                  ldapConf.given_name
+        });
+        NamingEnumeration<SearchResult> searchRes= getDirContext().search(ldapConf.peopleDN, filter, controls);
+        return searchRes == null ? null : searchRes;
+    }
+
+    String getSearchResult(ArrayList<? extends SearchResult> searchResultArray) throws NamingException {
+      String uid ="";
+      if(searchResultArray.size() == 1){
+        Attributes attributes = searchResultArray.get(0).getAttributes();
+        NamingEnumeration<? extends Attribute> attrs = attributes.getAll();
+        uid = (String) attributes.get("uid").get();
+        //afficher sur 1 seule ligne les infos LDAP
+        String queryresult= "";
+        while (attrs.hasMore()) {
+            Attribute attr = attrs.next();
+            queryresult = queryresult + attr;
+            queryresult = queryresult + "\t";
+        }
+        log.info("LDAP entry found  "+queryresult);
+      }else{
+        for(SearchResult searchResult:  searchResultArray){
+          Attributes attributes = searchResult.getAttributes();
+          NamingEnumeration<? extends Attribute> attrs = attributes.getAll();
+          String queryresult= "";
+          while (attrs.hasMore()) {
+              Attribute attr = attrs.next();
+              queryresult = queryresult + attr;
+              queryresult = queryresult + "\t";
+          }
+          log.info("LDAP entry found  "+queryresult);
+        }
+      }
+      return uid;
     }
 
 }
--- src/main/java/org/esupportail/claExternalID/Main.java
+++ src/main/java/org/esupportail/claExternalID/Main.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.SearchResult;
 
 
 import org.apache.commons.logging.LogFactory;
@@ -14,7 +18,15 @@ import static org.esupportail.claExternalID.Utils.*;
 import com.google.gson.Gson;
 import java.io.IOException;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
+
 import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.Collections;
 
 @SuppressWarnings("serial")
 public class Main extends HttpServlet {           
@@ -29,7 +41,11 @@ public class Main extends HttpServlet {
             HttpSession session=request.getSession();
             String target=request.getParameter("target");
             String redirectUrl = conf.cas_base_url;
-            
+
+
+
+            ArrayList<String> fcAttrs = new ArrayList<String>() ;
+
             if(request.getServletPath().equals("/associate/")){
                 String externalID=(String)session.getAttribute("externalID");
                 String remoteUser=request.getRemoteUser();
@@ -42,21 +58,91 @@ public class Main extends HttpServlet {
                 } else {
                     log.debug("externalID "+externalID+" does not associate to LDAP id "+remoteUser);
                 }
-                
+
                 if(target!=null) {
                     redirectUrl = target;
                 }
-            
+
             } else {
                 String remoteUser = request.getParameter("principal");
+
+                /***** FC attrs ******/
+              String given_name = request.getParameter("given_name");
+              String email = request.getParameter("email");
+              String gender = request.getParameter("gender");
+              String family_name = request.getParameter("family_name");
+              String birthdate = request.getParameter("birthdate");
+              //String preferred_username = request.getParameter("preferred_username");
+
+
+              if (gender.equals("male"))
+                gender = "M.";
+                else
+                gender = "Mme";
+
+
+              String[] birthD = birthdate.split("-");
+              Calendar calendar = Calendar.getInstance();
+              int year = Integer.valueOf(birthD[0]);
+              int month = Integer.valueOf(birthD[1]);
+              int day = Integer.valueOf(birthD[2]);
+              calendar.set(year,month-1,day,0,0,0);
+              SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss'Z'");
+              birthdate = dateFormat.format(calendar.getTime());
+              log.debug(dateFormat.format(calendar.getTime()));
+
+              fcAttrs.add(given_name);
+              fcAttrs.add(email);
+              fcAttrs.add(gender);
+              fcAttrs.add(family_name);
+              fcAttrs.add(birthdate);
+            //fcAttrs.add(preferred_username);
+
+
+
+              for(String fcAttr : fcAttrs ){
+                  log.debug("fcAttr---"+fcAttr);
+              }
+              /******** ********/
                 session.setAttribute("externalID", remoteUser);
                 log.debug("On page '/' remoteUser="+remoteUser);
                 session.removeAttribute(org.jasig.cas.client.util.AbstractCasFilter.CONST_CAS_ASSERTION);
+                //Récupérer les attributs LDAP depuis le mail transmis par FC
+               //Puis faire la comparaison avec la pseudo-ID pivot (Nom de naissance, prénom, sexe, date de naissance) + mail  --> LDAP(sn, givenName,   ,up1BirthDay) --> FC(family_name, given_name, gender, birthdate)
                 if(target!=null && !target.contains(request.getRequestURL()) && !request.getRequestURL().toString().contains(target) && remoteUser!=null){
+                  String searchFilter = "(&("+conf.ldap.birthdate+"="+birthdate+")("+conf.ldap.mailPerso+"="+email+")("+conf.ldap.civility+"="+gender+")("+conf.ldap.family_name+"="+family_name+")("+conf.ldap.given_name+"="+given_name+"))";
+                 ArrayList<? extends SearchResult> res = Collections.list(ldap.getEntryForReconciliation(searchFilter));
+                 if(res.size() == 1){
+                      log.debug("Successful LDAP search, redirecting to target");
+                      String uid=ldap.getSearchResult(res);
+                      //Adding the FCsub to the corresponding LDAP entry then redirect to appropriate target
+                      if(uid!=null) {
+                          if(!ldap.hasAttribute(uid, conf.refId_attribute, remoteUser)){
+                              ldap.addAttribute(uid, conf.refId_attribute, remoteUser);
+                          }
+                      } else {
+                          log.debug("LDAP entry found has a uid which value is null");
+                      }
+
+
+                      if(target!=null) {
+                          redirectUrl = target;
+                      }
+
+                 }else{
+                     log.debug("Something went wrong during the LDAP search process");
+                     log.info("Here is data retrieved from FC idp :  given_name="+given_name+"  | email="+email+"  | gender="+gender+"  | family_name="+family_name+"  | birthdate="+birthdate);
+                     if(res.size() > 1){
+                       log.info("The different entries retrieved");
+                       ldap.getSearchResult(res);
+                     }else{
+                       log.info("No LDAP entries matching with provided data");
+                     }
                     redirectUrl = "associate/?target=" + target;
+                  }
                 }
             }
-            
+
 	    log.debug("redirectUrl="+redirectUrl);
             if(redirectUrl == conf.cas_base_url){
                 log.debug("No valid page or arguments, redirect to CAS");
